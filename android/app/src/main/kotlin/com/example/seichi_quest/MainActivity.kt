6c7ab27a042f662e36755146c5dcbce62df23918
rainbowwing0603-blip <rainbowwing0603@gmail.com>
2026-08-22 10:05:45 +0900

    Show non-intrusive AdMob banner above bottom navigation

--- android/app/src/main/kotlin/com/example/seichi_quest/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/seichi_quest/MainActivity.kt
@@ -1,5 +1,41 @@
 package com.example.seichi_quest
 
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.FrameLayout
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private var adView: AdView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val banner = AdView(this).apply {
+            setAdSize(AdSize.BANNER)
+            adUnitId = "ca-app-pub-1391846841313915/2597290432"
+            loadAd(AdRequest.Builder().build())
+        }
+
+        adView = banner
+
+        val params = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+        ).apply {
+            gravity = Gravity.BOTTOM
+            bottomMargin = (80 * resources.displayMetrics.density).toInt()
+        }
+
+        addContentView(banner, params)
+    }
+
+    override fun onDestroy() {
+        adView?.destroy()
+        adView = null
+        super.onDestroy()
+    }
+}
