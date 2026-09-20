7b61a61f58dd5ac4b3c58a4fc16fd920e2ae4b96
rainbowwing0603-blip <rainbowwing0603@gmail.com>
2026-09-21 08:34:18 +0900

    refactor: make collection progress content agnostic

--- lib/services/collection_progress_policy.dart
+++ lib/services/collection_progress_policy.dart
@@ -1,17 +1,27 @@
-import '../models/seichi.dart';
+import '../models/quest_destination.dart';
 
 class CollectionProgressPolicy {
   const CollectionProgressPolicy();
 
-  int validCollectedCount({
-    required List<Seichi> seichiList,
+  int validCollectedCount<T extends QuestDestination>({
+    required Iterable<T> destinations,
     required Set<String> collectedIds,
   }) {
-    if (seichiList.isEmpty) {
+    if (destinations.isEmpty) {
       return 0;
     }
 
-    final validIds = seichiList.map((seichi) => seichi.id).toSet();
+    final validIds = destinations.map((destination) => destination.id).toSet();
     return collectedIds.where(validIds.contains).length;
+  }
+
+  int validCollectedCountForLegacy<T extends QuestDestination>({
+    required Iterable<T> seichiList,
+    required Set<String> collectedIds,
+  }) {
+    return validCollectedCount(
+      destinations: seichiList,
+      collectedIds: collectedIds,
+    );
   }
 }
