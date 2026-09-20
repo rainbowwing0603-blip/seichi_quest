fcd5e367f8767ecaa8b836581abbe62edfba5d6a
rainbowwing0603-blip <rainbowwing0603@gmail.com>
2026-09-21 08:34:27 +0900

    fix: preserve collection progress call compatibility

--- lib/services/collection_progress_policy.dart
+++ lib/services/collection_progress_policy.dart
@@ -4,24 +4,14 @@ class CollectionProgressPolicy {
   const CollectionProgressPolicy();
 
   int validCollectedCount<T extends QuestDestination>({
-    required Iterable<T> destinations,
-    required Set<String> collectedIds,
-  }) {
-    if (destinations.isEmpty) {
-      return 0;
-    }
-
-    final validIds = destinations.map((destination) => destination.id).toSet();
-    return collectedIds.where(validIds.contains).length;
-  }
-
-  int validCollectedCountForLegacy<T extends QuestDestination>({
     required Iterable<T> seichiList,
     required Set<String> collectedIds,
   }) {
-    return validCollectedCount(
-      destinations: seichiList,
-      collectedIds: collectedIds,
-    );
+    if (seichiList.isEmpty) {
+      return 0;
+    }
+
+    final validIds = seichiList.map((destination) => destination.id).toSet();
+    return collectedIds.where(validIds.contains).length;
   }
 }
