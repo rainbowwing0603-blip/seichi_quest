bce37fb8749e9e55566bb67d6c01c718475d756d
rainbowwing0603-blip <rainbowwing0603@gmail.com>
2026-09-21 00:05:58 +0900

    fix: remove obsolete collection count helper

--- lib/services/collection_apply_policy.dart
+++ lib/services/collection_apply_policy.dart
@@ -91,15 +91,4 @@ class CollectionApplyPolicy {
     );
   }
 
-  int progressPolicy.validCollectedCount({
-    required List<Seichi> seichiList,
-    required Set<String> collectedIds,
-  }) {
-    if (seichiList.isEmpty) {
-      return 0;
-    }
-
-    final validIds = seichiList.map((seichi) => seichi.id).toSet();
-    return collectedIds.where(validIds.contains).length;
-  }
 }
