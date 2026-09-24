c6ecd072188538b2ae47c70c8904a44e0bc84e8a
rainbowwing0603-blip <rainbowwing0603@gmail.com>
2026-09-24 19:34:37 +0900

    refactor: move UI collection flow to QuestItem

--- lib/services/collection_apply_policy.dart
+++ lib/services/collection_apply_policy.dart
@@ -1,5 +1,5 @@
 import '../models/achievement.dart';
-import '../models/seichi.dart';
+import '../models/quest_item.dart';
 import 'achievement_service.dart';
 import 'collection_progress_policy.dart';
 
@@ -11,7 +11,7 @@ class CollectionApplyPlan {
     required this.didCompleteQuest,
   });
 
-  final List<Seichi> newlyCollectedSeichi;
+  final List<QuestItem> newlyCollectedSeichi;
   final Set<String> newCollectedIds;
   final List<Achievement> newlyUnlockedAchievements;
   final bool didCompleteQuest;
@@ -29,7 +29,7 @@ class CollectionApplyPolicy {
   CollectionApplyPlan plan({
     required String currentEventId,
     required List<Map<String, dynamic>> collectedRows,
-    required List<Seichi> seichiList,
+    required List<QuestItem> seichiList,
     required Set<String> collectedIds,
     required List<Achievement> eventAchievements,
   }) {
@@ -45,7 +45,7 @@ class CollectionApplyPolicy {
         .where((id) => id.isNotEmpty)
         .toSet();
 
-    final newlyCollectedSeichi = seichiList
+    final newlyCollectedQuestItem = seichiList
         .where(
           (item) =>
               collectedEventContentIds.contains(item.id) &&
