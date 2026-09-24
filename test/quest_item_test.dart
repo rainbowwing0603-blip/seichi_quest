84bd2dbdc5107fc7ab3992a14e42081b255f80fe
rainbowwing0603-blip <rainbowwing0603@gmail.com>
2026-09-24 19:44:52 +0900

    test: remove legacy seichi id expectations

--- test/quest_item_test.dart
+++ test/quest_item_test.dart
@@ -20,14 +20,13 @@ void main() {
     );
 
     expect(item.contentKey, 'shop-a');
-    expect(item.legacySeichiId, isNull);
     expect(item.legacyCard, isNull);
     expect(item.legacyReading, isNull);
   });
 
   test('legacy karuta metadata is optional compatibility information', () {
     const item = QuestItem(
-      id: 'legacy-seichi-1',
+      id: 'event-content-1',
       eventContentId: 'event-content-1',
       contentId: 'content-1',
       placeId: 'place-1',
@@ -41,13 +40,11 @@ void main() {
       displayOrder: 1,
       isActive: true,
       contentMetadata: {
-        'legacy_seichi_id': 'legacy-seichi-1',
         'card': 'あ',
         'reading': 'あ',
       },
     );
 
-    expect(item.legacySeichiId, 'legacy-seichi-1');
     expect(item.legacyCard, 'あ');
     expect(item.legacyReading, 'あ');
   });
